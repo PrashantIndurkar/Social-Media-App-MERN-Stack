49b1f2b87403831e2cddd04da3dfd5f378730fef
PrashantIndurkar <prashantri2000@gmail.com>
2022-03-04 03:59:23 +0530

    created post and user Schema

---
+++ Notes/Episode2.md
@@ -0,0 +1,4 @@
+1. Created modules folder with Files Posts.js and User.js
+2. this are the Schema for our application "what is a Schema? -> Everything in Mongoose starts with a Schema. Each schema maps to a MongoDB collection and defines the shape of the documents within that collection. "
+3. first require mongoose & using new mongoose.Schema() create a Schema for the post and user and name that Schema ex PostSchema, UserSchema etc
+4. after that exports
---
+++ backend/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please Enter An Email"],
+		unique: [true, "Email already exists"],
+	},
+  avatar:{
+    public_id:String,
+    url:String
+  }
+	password: {
+		type: String,
+		required: [true, "please enter a password"],
+		minlength: [6, "Password must be at least 6 characters"],
+		select: false,
+	},
+	posts: [
+		{
+			type: mongoose.Schema.Type.ObjectId,
+			ref: "Post",
+		},
+	],
+	followers: [
+		{
+			type: mongoose.Schema.Type.ObjectId,
+			ref: "User",
+		},
+	],
+	following: [
+		{
+			type: mongoose.Schema.Type.ObjectId,
+			ref: "User",
+		},
+	],
+});
+
+module.exports = mongoose.model("User", userSchema);
